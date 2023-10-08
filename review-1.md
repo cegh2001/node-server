71a3b6af0d02f4cf5c32d76a4e928991d56f414e
Carlos Gonzalez <cargonzalez0601@gmail.com>
2023-10-07 20:17:55 -0400

    Ultima parte

---
+++ review-1.md
@@ -0,0 +1,18 @@
+Esto es un "Gestor de Tareas de uso simple," es una herramienta diseñada para ayudarte a mantener un registro organizado de tus tareas diarias y pendientes de una manera simple y eficaz.
+
+¿Cuáles son las funcionalidades más importantes y por qué los usuarios las usarían?
+
+1. Lista de Tareas Intuitiva
+Una de las características más destacadas es su interfaz de usuario intuitiva y fácil de usar. Los usuarios pueden ver de un vistazo todas sus tareas pendientes, lo que simplifica la gestión y el seguimiento de sus responsabilidades diarias. La simplicidad es la clave.
+
+2. Descripciones Detalladas
+Cada tarea puede incluir una descripción detallada. Esto permite a los usuarios anotar información importante sobre la tarea, como instrucciones específicas, plazos, prioridades y cualquier otro detalle relevante. 
+
+3. Estado de Tareas
+El Gestor permite a los usuarios marcar sus tareas como "completadas" o "pendientes". Esta característica proporciona una visión clara del progreso y ayuda a los usuarios a enfocarse en las tareas que aún deben completarse. Es una forma efectiva de mantener el control y mantenerse al tanto de las responsabilidades.
+
+4. Acceso desde Cualquier Lugar
+Nuestro Gestor de Tareas es una aplicación web accesible desde cualquier dispositivo con acceso a Internet. Los usuarios pueden gestionar sus tareas desde su computadora de trabajo, su teléfono móvil o su tableta. Esto brinda una flexibilidad invaluable.
+
+5. Sin Conocimientos Técnicos Requeridos
+Una de las ventajas clave de nuestro producto es que no se requieren conocimientos técnicos para su uso. La interfaz es amigable, lo que significa que cualquier persona, independientemente de su experiencia tecnológica, puede comenzar a usarlo de inmediato.
--- script.js
+++ script.js
@@ -1,119 +1,22 @@
-const readline = require("readline-sync");
+const http = require("http");
 
-// Array para almacenar las tareas
-const tasks = [];
+const port = 8080; 
+const tasks = [
+  { id: 1, description: "Hacer comida", completed: false },
+  { id: 2, description: "Estudiar JavaScript", completed: true },
+  { id: 3, description: "Estudiar Python", completed: true },
+];
 
-// Función para mostrar la lista de tareas
-function showTasks() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (tasks.length === 0) {
-        console.log("La lista de tareas está vacía.");
-      } else {
-        console.log("Lista de tareas:");
-        tasks.forEach((task, index) => {
-          const status = task.completed ? "[Completada]" : "[Pendiente]";
-          console.log(`${index + 1}. ${status} ${task.description}`);
-        });
-      }
-      resolve();
-    }, 1000); // Simulación de retardo
-  });
-}
-
-// Función para agregar una nueva tarea
-function addTask(description) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      tasks.push({ description, completed: false });
-      console.log("Tarea agregada.");
-      resolve();
-    }, 1000); // Simulación de retardo
-  });
-}
-
-// Función para eliminar una tarea
-function deleteTask(taskIndex) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (taskIndex >= 0 && taskIndex < tasks.length) {
-        tasks.splice(taskIndex, 1);
-        console.log("Tarea eliminada.");
-        resolve();
-      } else {
-        reject("Número de tarea no válido.");
-      }
-    }, 1000); // Simulación de retardo
-  });
-}
-
-// Función para marcar una tarea como completada
-function completeTask(taskIndex) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (taskIndex >= 0 && taskIndex < tasks.length) {
-        tasks[taskIndex].completed = true;
-        console.log("Tarea marcada como completada.");
-        resolve();
-      } else {
-        reject("Número de tarea no válido.");
-      }
-    }, 1000); // Simulación de retardo
-  });
-}
-
-// Función principal
-async function main() {
-  while (true) {
-    console.log("\nOpciones:");
-    console.log("1. Mostrar tareas");
-    console.log("2. Agregar tarea");
-    console.log("3. Eliminar tarea");
-    console.log("4. Marcar tarea como completada");
-    console.log("5. Salir");
-
-    const choice = readline.questionInt(
-      "Ingrese el número de la opción que desea ejecutar: "
-    );
-
-    switch (choice) {
-      case 1:
-        await showTasks();
-        break;
-      case 2:
-        const description = readline.question(
-          "Ingrese la descripción de la tarea: "
-        );
-        await addTask(description);
-        break;
-      case 3:
-        const deleteIndex =
-          readline.questionInt(
-            "Ingrese el número de la tarea que desea eliminar: "
-          ) - 1;
-        try {
-          await deleteTask(deleteIndex);
-        } catch (error) {
-          console.log(error);
-        }
-        break;
-      case 4:
-        const completeIndex =
-          readline.questionInt(
-            "Ingrese el número de la tarea que desea marcar como completada: "
-          ) - 1;
-        try {
-          await completeTask(completeIndex);
-        } catch (error) {
-          console.log(error);
-        }
-        break;
-      case 5:
-        process.exit(0);
-      default:
-        console.log("Opción no válida.");
-    }
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/tasks") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(tasks));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   }
-}
+});
 
-main();
+server.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
